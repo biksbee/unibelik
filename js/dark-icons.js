7419a111c6d9e36136616da2b78b209c96571847
biksbee <77857837+biksbee@users.noreply.github.com>
2021-02-05 15:50:07 +0300

    Delete dark-icons.js

--- js/dark-icons.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener("load", function load() {
-//       var imgs=["myImage1", "myImage2", "myImage3"];
- 
-//       imgs.forEach(function(elementId){
-//          document.getElementById(elementId).src='/2.png';
-//       });
-//    }, false);
