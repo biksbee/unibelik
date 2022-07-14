68c40c3f3607e6239606ed66fe3ab7a4211daa0a
biksbee <77857837+biksbee@users.noreply.github.com>
2022-07-14 20:00:09 +0300

    Update menu-script.js

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,4 +1,19 @@
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+        $('.scroll').addClass("sticky");
+    }
+    else{	
+        $('.scroll').removeClass("sticky");
+    }
+});
 $(document).ready(function() {
     $('.lan').click(function(event) {
         $('.lanpole').toggleClass('active');
